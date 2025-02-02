db2f600d18c51e8d19b035a1e45339632f03e587
jeongminchan <104063175+MinchoLover@users.noreply.github.com>
2025-02-02 19:35:56 +0900

    [feat]: 헤더에 검색기능을 추가했다

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,65 @@
 import styled from "styled-components";
 import { useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 const Header = () => {
   // SearchBarInput에 포커스할 시, searchIcon을 true로 바꿈
   const [searchLogo, setSearchLogo] = useState(false);
+  // 음석인식 훅
+  const { transcript, listening } = useSpeechRecognition();
+
+  const toggleListening = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening({ 
+        continuous: true,
+        interimResults: true,
+        language: 'ko-KR'
+      });
+    }
+  };
 
   return (
     <>
       <HeaderWrapper>
         <HeaderContainer>
 
+          {/* 사이드바 + 유튜브 로고 */}
           <HeaderLogo>
             <img src="/icons/sidebarIcon.svg" alt="Sidebar Logo" style={({width:"24px"})}/>
             <img src="/icons/youtubeLogo.svg" alt="YouTube Logo" style={({width:"101px",height:"20"})}/>
           </HeaderLogo>
 
           <SearchBarLayout>
+            {/* 검색바 */}
             <SearchBar>
-              <SearchBarLeft searchLogo={searchLogo}>
+              {/* 여기서 돋보기 아이콘이 true가 되면 SearchBar의 왼쪽의 width가 늘어나야함 */}
+              <SearchBarLeft>
                 {searchLogo && (
-                  <img src="/icons/searchIcon.svg" alt="Search Icon" style={({width:"18px", height:"18px"})}/>
-                )}
-                <SearchBarInput
-                  placeholder="검색"
-                  onFocus={() => setSearchLogo(true)}
-                  onBlur={() => setSearchLogo(false)}
-                />
-              </SearchBarLeft>
+                    <img src="/icons/searchIcon.svg" alt="Search Icon" style={({width:"18px", height:"18px"})}/>
+                  )}
+                  <SearchBarInput
+                    placeholder="검색"
+                    onFocus={() => setSearchLogo(true)}
+                    onBlur={() => setSearchLogo(false)}
+                    value={transcript}
+                  />
+                </SearchBarLeft>
               <SearchBarRight>
                 <img src="/icons/searchIcon.svg" alt="Search Icon" style={({width:"18px", height:"18px"})}/>
               </SearchBarRight>
+            <SpeechButton onClick={toggleListening}>
+              {listening ? "음성인식중지" : "음성인식시작"}
+            </SpeechButton>
             </SearchBar>
           </SearchBarLayout>
 
-          <h1>만들기 + 알림 + 유저 프로필</h1>
+          <RightSection>
+            <Make>+ 만들기</Make>
+            <Notification>알림</Notification>
+            <Profile>프로필</Profile>
+          </RightSection>
         </HeaderContainer>
       </HeaderWrapper>
     </>
@@ -47,7 +73,7 @@ const HeaderWrapper = styled.header`
   top: 0;
   left: 0;
   width: 100%;
-  /* background-color: blue; */
+  /* background-color: black; */
   z-index: 100;
 `;
 
@@ -77,7 +103,6 @@ const SearchBar = styled.div`
   /* align-items: center; */
 `;
 
-
 const SearchBarLeft = styled.div`
   flex: 1;
   display: flex;
@@ -85,16 +110,17 @@ const SearchBarLeft = styled.div`
   border: 1px solid #c6c6c6;
   margin-left: 32px;
   border-radius: 40px 0 0 40px;
+  padding-left: 10px;
 `;
 
 const SearchBarInput = styled.input`
   position: relative;
   width: 100%;
   border: none;
-
-  ::placeholder {
-    font-size: 1.6rem;
-    color: #c6c6c6;
+  height: 20px;
+  
+  &::placeholder {
+    color: inherit;
   }
   &:focus {
     outline: none;
@@ -102,8 +128,42 @@ const SearchBarInput = styled.input`
 `;
 
 const SearchBarRight = styled.div`
+  width: 60px;
   border: 1px solid #c6c6c6;
   border-radius: 0 40px 40px 0;
   display: flex;
   align-items: center;
+  justify-content: center;
+`;
+
+const SpeechButton = styled.button`
+  border-radius: 40px;
+  border: none;
+`;
+const RightSection = styled.div`
+  display: flex;
+  height: 40px;
+`;
+
+const Make = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  border: 1px solid #c6c6c6;
+  border-radius: 40px;
+`;
+
+const Notification = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  border: 1px solid #c6c6c6;
+  border-radius: 40px;
+`;
+
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid #c6c6c6;
+  border-radius: 40px;
 `;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,25 +3,6 @@ const HomePage = () => {
   return (
     <>
       <h1>Home Page1</h1>
-      <h1>Home Page2</h1>
-      <h1>Home Page3</h1>
-      <h1>Home Page4</h1>
-      <h1>Home Page5</h1>
-      <h1>Home Page6</h1>
-      <h1>Home Page7</h1>
-      <h1>Home Page8</h1>
-      <h1>Home Page9</h1>
-      <h1>Home Page10</h1>
-      <h1>Home Page11</h1>
-      <h1>Home Page12</h1>
-      <h1>Home Page13</h1>
-      <h1>Home Page14</h1>
-      <h1>Home Page15</h1>
-      <h1>Home Page16</h1>
-      <h1>Home Page17</h1>
-      <h1>Home Page18</h1>
-      <h1>Home Page19</h1>
-      <h1>Home Page20</h1>
     </>
   );
 }
